5e0afd0f0a444cca753c23d56fb53ac63461afe5
moktin <moktin@gmail.com>
2012-04-06 20:51:10 +0200

    prepare to deploy heroku

--- config/application.rb
+++ config/application.rb
@@ -56,6 +56,6 @@ module Gallery
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
     config.assets.initialize_on_precompile = false
-    config.assets.precompile += %w( jquery-ui-1.8.18.custom.css jquery-ui-1.8.18.custom.min.js admin/admin.js)
+    config.assets.precompile += %w( jquery-ui-1.8.18.custom.css jquery-ui-1.8.18.custom.min.js admin/admin.js ckeditor/ckeditor.js)
   end
 end
