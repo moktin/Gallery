25921303bf00741506bd99696063c3a795e14860
moktin <moktin@gmail.com>
2012-04-06 20:14:37 +0200

    prepare to deploy heroku

--- config/application.rb
+++ config/application.rb
@@ -56,5 +56,6 @@ module Gallery
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
     config.assets.initialize_on_precompile = false
+    config.assets.precompile += %w( jquery-ui-1.8.18.custom.css )
   end
 end
